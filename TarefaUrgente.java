6c0e33634e95869c99cabe7cd1607a0857667b51
Henrique <netohehnique02@gmail.com>
2024-09-18 09:24:43 -0300

    Tarefa Urgente exibirDetalhes()

--- TarefaUrgente.java
+++ TarefaUrgente.java
@@ -1,10 +1,25 @@
 public class TarefaUrgente extends Tarefa { // herança de classe
-    private String razaoPrioridade;
+    private String razaoUrgencia;
     
-    public TarefaUrgente(String titulo, String descricao, String prazo, String razaoPrioridade) {
+    public TarefaUrgente(String titulo, String descricao, String prazo, String razaoUrgencia) {
         super(titulo, descricao, prazo, 5); // usando o construtor da classe Tarefa
-        this.razaoPrioridade = razaoPrioridade;
-
+        this.razaoPrioridade = razaoUrgencia;
     }
 
+    // métodos
+
+    public getRazaoUrgencia(){
+        return this.razaoUrgencia;
+    }
+
+    public setRazaoUrgencia(String razaoUrgencia){
+        this.razaoUrgencia = razaoUrgencia;
+    }
+
+    @Override
+    public void exibirDetalhes(){
+        super exibirDetalhes;
+        System.out.pintln("Razão da Urgência: " + this.razaoUrgencia);
+
+    }
 }
